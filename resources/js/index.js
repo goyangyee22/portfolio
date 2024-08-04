7e2b70d5bf59c5b068345f75c357cf17959ed9a3
LeeJh <4447447@naver.com>
2024-08-04 16:01:29 +0900

    update

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,4 +1,24 @@
-const comingSoon = document.querySelector(".comingSoon");
-comingSoon.addEventListener("click", function () {
-  alert("Coming soon!");
+// ./resources/js/index.js
+document.addEventListener('DOMContentLoaded', () => {
+  const descriptions = document.querySelectorAll('#main2 .description');
+  const links = document.querySelectorAll('#main1 .fillUpBtn');
+
+  links.forEach(link => {
+    link.addEventListener('mouseover', (event) => {
+      const descriptionId = event.target.getAttribute('data-description');
+      descriptions.forEach(desc => {
+        if (desc.id === descriptionId) {
+          desc.style.display = 'block'; // 설명 표시
+        } else {
+          desc.style.display = 'none'; // 나머지 설명 숨기기
+        }
+      });
+    });
+
+    link.addEventListener('mouseout', () => {
+      descriptions.forEach(desc => {
+        desc.style.display = 'none'; // 모든 설명 숨기기
+      });
+    });
+  });
 });
